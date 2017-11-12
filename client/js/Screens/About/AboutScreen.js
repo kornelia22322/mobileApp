fe456d3c88a90f886f47cd375a73a711792f2bb6
korneliarohullko <korneliarohullko@gmail.com>
2017-11-12 01:01:57 +0100

    Adding about-us page

--- client/js/Screens/About/AboutScreen.js
+++ client/js/Screens/About/AboutScreen.js
@@ -1,33 +1,73 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { SectionList, ScrollView, Text } from 'react-native';
 import DrawerIcon from '../../Drawer/DrawerIcon';
 import Toolbar from '../Base/Toolbar';
 import Scene from '../../GlamorousComponents/Scene';
+import Config from '../../Utils/Config';
 import DrawerComponent from '../../GlamorousComponents/DrawerComponent';
+import Card from '../../GlamorousComponents/Card';
+import Header from '../../GlamorousComponents/Header';
+import Body from '../../GlamorousComponents/Body';
 import { IntlText } from '../../Translation/IntlText';
 
-const ContactScreen = ({ navigation }) => (
-  <Scene>
-    <Toolbar
-      title="drawer.contact"
-      leftButton={{
-        icon: 'menu',
-        onPress: () => navigation.navigate('DrawerOpen')
-      }}
-    />
-    <ScrollView>
-      <Text>
-        Tu będzie ContactScreen
-      </Text>
-    </ScrollView>
-  </Scene>
-);
-ContactScreen.navigationOptions = {
-  drawerLabel: <DrawerComponent>
-    <IntlText id="drawer.contact"/>
-  </DrawerComponent>,
-  drawerIcon:
-    <DrawerIcon name="mail"/>
+
+class AboutScreen extends React.Component {
+    render() {
+        return (
+            <Scene style={{ flex: 1 }}>
+              <Toolbar
+                  title="drawer.about"
+                  leftButton={{
+                      icon: 'menu',
+                      onPress: () => this.props.navigation.navigate('DrawerOpen')
+                  }}
+              />
+
+              <ScrollView>
+                <Card>
+                  <Header>
+                    <IntlText id={'about.sfiHeader'}/>
+                  </Header>
+                  <Body>
+                  <IntlText id={'about.sfiBody'}/>
+                  </Body>
+                </Card>
+                <Card>
+                  <Header>
+                    <IntlText id={'about.appHeader'}/>
+                  </Header>
+                  <Body>
+                  <IntlText id={'about.appBody'}/>
+                  </Body>
+                  <SectionList
+                      sections={[
+                          { title: 'Developers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Developers' },
+                          { title: 'Designers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Designers' },
+                          { title: 'Support', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Support' }
+                      ]}
+                      renderItem={({item}) => <Text style = {{ fontSize: Config.mediumFontSize, color: Config.textColorSupportive, fontWeight: '100' }} >{item.key}</Text>}
+                      renderSectionHeader={({section}) => <Text style = {{ fontSize: Config.mediumFontSize, fontWeight: 'bold', marginTop: 5, color: Config.textColorSupportive }}>{section.title}</Text>}
+                  /></Card>
+                <Card>
+                  <Header>
+                    <IntlText id={'about.contactHeader'}/>
+                  </Header>
+                  <Body style={{marginBottom: 20}}>
+                  <IntlText id={'about.contactBody'}/>
+                  </Body>
+                </Card>
+              </ScrollView>
+            </Scene>
+        );
+    }
+}
+
+AboutScreen.navigationOptions = {
+    drawerLabel: <DrawerComponent>
+      <IntlText id="drawer.about"/>
+    </DrawerComponent>,
+    drawerIcon:
+        <DrawerIcon name="info"/>
 };
 
-export default ContactScreen;
+export default AboutScreen;
--- client/js/Translation/Translations/pl.js
+++ client/js/Translation/Translations/pl.js
@@ -4,7 +4,7 @@ export default {
     news: 'Wiadomości',
     codes: 'Kody',
     settings: 'Ustawienia',
-    contact: 'Kontakt'
+    about: 'O nas'
   },
   languages: {
     pl: 'Polski',
@@ -13,5 +13,13 @@ export default {
   settings: {
     changeLanguage: 'Zmień język:'
   },
-  greeting: 'Hej!'
+  greeting: 'Hej!',
+  about: {
+    sfiHeader: 'SFI - Informacje',
+    sfiBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    appHeader: 'Aplikacja SFI',
+    appBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    contactHeader: 'Kontakt',
+    contactBody: 'Lorem ipsum dolor sit amet'
+  }
 };
