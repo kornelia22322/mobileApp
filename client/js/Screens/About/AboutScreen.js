14e40f2b6f4d0273d601747d74fe13f30f62f55f
korneliarohullko <korneliarohullko@gmail.com>
2017-11-12 12:08:03 +0100

    identations fixed and id labels repaired

--- client/js/Screens/About/AboutScreen.js
+++ client/js/Screens/About/AboutScreen.js
@@ -12,62 +12,63 @@ import { IntlText } from '../../Translation/IntlText';
 
 
 class AboutScreen extends React.Component {
-    render() {
-        return (
-            <Scene style={{ flex: 1 }}>
-              <Toolbar
-                  title="drawer.about"
-                  leftButton={{
-                      icon: 'menu',
-                      onPress: () => this.props.navigation.navigate('DrawerOpen')
-                  }}
-              />
+  render() {
+    return (
+      <Scene style={{ flex: 1 }}>
+        <Toolbar
+          title="drawer.about"
+          leftButton={{
+            icon: 'menu',
+            onPress: () => this.props.navigation.navigate('DrawerOpen')
+          }}
+        />
 
-              <ScrollView>
-                <Card>
-                  <Header>
-                    <IntlText id={'about.sfiHeader'}/>
-                  </Header>
-                  <Body>
-                  <IntlText id={'about.sfiBody'}/>
-                  </Body>
-                </Card>
-                <Card>
-                  <Header>
-                    <IntlText id={'about.appHeader'}/>
-                  </Header>
-                  <Body>
-                  <IntlText id={'about.appBody'}/>
-                  </Body>
-                  <SectionList
-                      sections={[
-                          { title: 'Developers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Developers' },
-                          { title: 'Designers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Designers' },
-                          { title: 'Support', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Support' }
-                      ]}
-                      renderItem={({item}) => <Text style = {{ fontSize: Config.mediumFontSize, color: Config.textColorSupportive, fontWeight: '100' }} >{item.key}</Text>}
-                      renderSectionHeader={({section}) => <Text style = {{ fontSize: Config.mediumFontSize, fontWeight: 'bold', marginTop: 5, color: Config.textColorSupportive }}>{section.title}</Text>}
-                  /></Card>
-                <Card>
-                  <Header>
-                    <IntlText id={'about.contactHeader'}/>
-                  </Header>
-                  <Body style={{marginBottom: 20}}>
-                  <IntlText id={'about.contactBody'}/>
-                  </Body>
-                </Card>
-              </ScrollView>
-            </Scene>
-        );
-    }
+        <ScrollView>
+          <Card>
+            <Header>
+              <IntlText id="about.sfiHeader"/>
+            </Header>
+            <Body>
+              <IntlText id="about.sfiBody"/>
+            </Body>
+          </Card>
+          <Card>
+            <Header>
+              <IntlText id="about.appHeader"/>
+            </Header>
+            <Body>
+              <IntlText id="about.appBody"/>
+            </Body>
+            <SectionList
+              sections={[
+                { title: 'Developers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Developers' },
+                { title: 'Designers', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Designers' },
+                { title: 'Support', data: [{ key:'John Smith'}, {key: 'John Smith'}, {key: 'John Smith' }, { key: 'John Smith' }], key: 'Support' }
+              ]}
+              renderItem={({item}) => <Text style = {{ fontSize: Config.mediumFontSize, color: Config.textColorSupportive, fontWeight: '100' }} >{item.key}</Text>}
+              renderSectionHeader={({section}) => <Text style = {{ fontSize: Config.mediumFontSize, fontWeight: 'bold', marginTop: 5, color: Config.textColorSupportive }}>{section.title}</Text>}
+            />
+          </Card>
+          <Card>
+            <Header>
+              <IntlText id="about.contactHeader"/>
+            </Header>
+            <Body style={{marginBottom: 20}}>
+              <IntlText id="about.contactBody"/>
+            </Body>
+          </Card>
+        </ScrollView>
+      </Scene>
+    );
+  }
 }
 
 AboutScreen.navigationOptions = {
-    drawerLabel: <DrawerComponent>
-      <IntlText id="drawer.about"/>
-    </DrawerComponent>,
-    drawerIcon:
-        <DrawerIcon name="info"/>
+  drawerLabel: <DrawerComponent>
+    <IntlText id="drawer.about"/>
+  </DrawerComponent>,
+  drawerIcon:
+    <DrawerIcon name="info"/>
 };
 
 export default AboutScreen;
